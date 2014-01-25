fe126e5d853a6c2804d0ed5f7b31c3e5c0fbc05e
Nicholas Nelson <nnelson8675@gmail.com>
2014-01-25 14:44:35 -0800

    text edit field and button added to activity_main window layout

--- gen/com/example/classlocus/R.java
+++ gen/com/example/classlocus/R.java
@@ -24,7 +24,8 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080001;
+        public static final int edit_message=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -33,9 +34,11 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
+        public static final int action_settings=0x7f050003;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int button_send=0x7f050002;
+        public static final int edit_message=0x7f050001;
+        public static final int title_activity_main=0x7f050004;
     }
     public static final class style {
         /** 
