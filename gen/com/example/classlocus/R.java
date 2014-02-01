55687250ae9759ccad0a607760d238cadb26ddc6
Nicholas Nelson <nnelson8675@gmail.com>
2014-02-01 11:41:03 -0800

    building & room search fields added, layout updated

--- gen/com/example/classlocus/R.java
+++ gen/com/example/classlocus/R.java
@@ -24,8 +24,11 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
-        public static final int edit_message=0x7f080000;
+        public static final int action_settings=0x7f080004;
+        public static final int building_field=0x7f080000;
+        public static final int building_input=0x7f080001;
+        public static final int room_field=0x7f080002;
+        public static final int room_input=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -34,11 +37,14 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050003;
+        public static final int action_settings=0x7f050006;
         public static final int app_name=0x7f050000;
-        public static final int button_send=0x7f050002;
-        public static final int edit_message=0x7f050001;
-        public static final int title_activity_main=0x7f050004;
+        public static final int button_search=0x7f050005;
+        public static final int hint_building=0x7f050002;
+        public static final int hint_room=0x7f050004;
+        public static final int title_activity_main=0x7f050007;
+        public static final int title_building=0x7f050001;
+        public static final int title_room=0x7f050003;
     }
     public static final class style {
         /** 
