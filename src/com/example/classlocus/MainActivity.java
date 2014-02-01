59e96b4065da7a68b29e5fe681d5e24a6a37b738
johnsbr4 <johnsbr4@onid.oregonstate.edu>
2014-02-01 15:05:46 -0800

    Added search intent

--- src/com/example/classlocus/MainActivity.java
+++ src/com/example/classlocus/MainActivity.java
@@ -2,14 +2,33 @@ package com.example.classlocus;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity{
+	
+	Button button;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		this.setContentView(R.layout.activity_main);
+		
+		button = (Button) this.findViewById(R.id.button_search);
+		button.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent searchIntent = getIntent();
+				if(Intent.ACTION_SEARCH.equals(searchIntent.getAction())) {
+					String query = searchIntent.getStringExtra(SearchManager.QUERY);
+					runSearch(query);
+				}
+			}
+		});
 	}
 
 	@Override
@@ -18,5 +37,5 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
+	
 }
