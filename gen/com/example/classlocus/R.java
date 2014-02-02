26b5bddec18f392712c69756981fcb5df50dcbd4
johnsbr4 <johnsbr4@onid.oregonstate.edu>
2014-02-01 16:08:23 -0800

    Added action bar search

--- gen/com/example/classlocus/R.java
+++ gen/com/example/classlocus/R.java
@@ -28,19 +28,23 @@ public final class R {
         public static final int building_field=0x7f090000;
         public static final int building_input=0x7f090001;
         public static final int button_search=0x7f090004;
+        public static final int help=0x7f090007;
         public static final int room_field=0x7f090002;
         public static final int room_input=0x7f090003;
+        public static final int search=0x7f090006;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
         public static final int main=0x7f080000;
+        public static final int options_menu=0x7f080001;
     }
     public static final class string {
         public static final int action_settings=0x7f060007;
         public static final int app_name=0x7f060000;
         public static final int button_search=0x7f060005;
+        public static final int help=0x7f060009;
         public static final int hint_building=0x7f060002;
         public static final int hint_room=0x7f060004;
         public static final int hint_search=0x7f060006;
