c01def3612d9c2b2102d41a1cab75902a1bf9277
Micah <micahh2@gmail.com>
2014-02-02 16:00:06 -0800

    Added a dummy building class that can be used.

--- src/com/example/classlocus/Building.java
+++ src/com/example/classlocus/Building.java
@@ -4,20 +4,28 @@ import android.database.sqlite.SQLiteDatabase.CursorFactory;
 
 import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
 
-public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
-	//Database Name
-	private static final String DB_NAME = "classlocus";
-	//Database version (We may have to update when we update the schema)
-	private static final int DB_Version = 1;
-	
-	//ORM instance
-	private  Dao<Building, Integer> buildingDao = "classlocus";
-	
-	public DatabaseHelper(Context context, String databaseName,
-			CursorFactory factory, int databaseVersion) {
-		super(context, databaseName, factory, databaseVersion);
-		// TODO Auto-generated constructor stub
+class coordinates {
+	public double lat;
+	public double lng;
+	public coordinates(double latitude, double longitude) {
+		lat = latitude;
+		lng = longitude;
 	}
+}
+
+public class Building {
+	private String name;
+	private String abrv;
+	private coordinates GPS;
+	private int buildingID;
+	private int parentLocation;
+	private boolean accessible;
 	
+	public Building(String buildingName, String abreviation, coordinates buildingGPS, int BID) {
+		name = buildingName;
+		abrv = abreviation;
+		GPS = buildingGPS;
+		buildingID = BID;
+	}
 	
 }
