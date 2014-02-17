9559885e2f0a041080999b4792da0da0fd2ebe54
Anthony <anthonymiller@outlook.com>
2014-02-16 16:52:21 -0800

    Revert "Attempted to get current location"
    This reverts commit c2df7c8654cc1c39d960d51f3b7bde92bbb55403.

--- src/com/example/classlocus/BuildingDetail.java
+++ src/com/example/classlocus/BuildingDetail.java
@@ -1,29 +1,16 @@
 package com.example.classlocus;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesClient;
-import com.google.android.gms.common.GooglePlayServicesUtil;
-import com.google.android.gms.location.LocationClient;
 import com.google.android.gms.maps.model.*;
 import com.google.maps.android.SphericalUtil;
 
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
-import android.widget.Toast;
 import android.support.v4.app.NavUtils;
 
-public class BuildingDetail extends Activity implements 
-GooglePlayServicesClient.ConnectionCallbacks, 
-GooglePlayServicesClient.OnConnectionFailedListener{
-	
-	LocationClient mLocationClient;
-	boolean isConnected = false;
+public class BuildingDetail extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +18,9 @@ GooglePlayServicesClient.OnConnectionFailedListener{
 		setContentView(R.layout.activity_building_detail);
 		// Show the Up button in the action bar.
 		setupActionBar();
-		mLocationClient = new LocationClient(this, this, this);
+		
+		TextView tv = (TextView) findViewById(R.id.detail_building_distance);
+		tv.setText(buildingDistance(new LatLng(2d, 2d), new LatLng(2d, 2d)));
 		
 	}
 
@@ -73,64 +62,5 @@ GooglePlayServicesClient.OnConnectionFailedListener{
 		String dString = String.valueOf(distance);
 		dString = dString + "m";
 		return dString;
-	}
-	
-	protected void onStart(){
-		super.onStart();
-		mLocationClient.connect();
-		while(!isConnected);
-		LatLng test = getCurrentLocation();
-		//TextView tv = (TextView) findViewById(R.id.detail_building_distance);
-		//tv.setText(buildingDistance(getCurrentLocation(), new LatLng(2d, 2d)));
-	}
-	
-	protected void onStop(){
-		super.onStop();
-		mLocationClient.disconnect();
-	}
-	
-	public LatLng getCurrentLocation(){
-		int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
-		switch (status){
-		case ConnectionResult.SUCCESS:
-			Location l = mLocationClient.getLastLocation();
-			return new LatLng(l.getLatitude(), l.getLongitude());
-		case ConnectionResult.SERVICE_MISSING:
-			Toast.makeText(this, "This device is not supported.", Toast.LENGTH_SHORT).show();
-	    	finish();
-			break;
-		case ConnectionResult.SERVICE_VERSION_UPDATE_REQUIRED:
-			Toast.makeText(this, "This device is not supported.", Toast.LENGTH_SHORT).show();
-	    	finish();
-			break;
-		case ConnectionResult.SERVICE_DISABLED:
-			Toast.makeText(this, "This device is not supported.", Toast.LENGTH_SHORT).show();
-	    	finish();
-			break;
-		case ConnectionResult.SERVICE_INVALID:
-			Toast.makeText(this, "This device is not supported.", Toast.LENGTH_SHORT).show();
-	    	finish();
-			break;
-		case ConnectionResult.DATE_INVALID:
-			Toast.makeText(this, "This device is not supported.", Toast.LENGTH_SHORT).show();
-	    	finish();
-			break;
-		}
-		return null;
-	}
-
-	@Override
-	public void onConnectionFailed(ConnectionResult arg0) {
-		Toast.makeText(this, "Connection Failed", Toast.LENGTH_SHORT).show();
-	}
-
-	@Override
-	public void onConnected(Bundle arg0) {
-		isConnected = true;
-	}
-
-	@Override
-	public void onDisconnected() {
-		isConnected = false;
 	}
 }
